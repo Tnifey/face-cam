b0ef496b104701902d7f0991bd6217f08959addf
Tnifey <tnifey@gmail.com>
2024-04-05 21:03:14 +0200

    probably add stream selector

--- face-cam.js
+++ face-cam.js
@@ -2,7 +2,7 @@ run();
 async function run() {
     const video = document.querySelector('video');
     if (!video) {
-        console.error('No video element found');
+        console.error('No video element found. Fault in the matrix.');
         return;
     }
 
@@ -12,18 +12,40 @@ async function run() {
         .catch((e) => void console.error(e) || [])
         .then((devices) => devices.filter((device) => device.kind === 'videoinput'));
 
-    if (!devices.length) {
+    if (!devices?.length) {
         console.error('No video devices found');
+        alert('No video devices found or permission denied.');
         return;
     }
 
     console.log('Found video devices:', devices);
 
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-        .then(stream => {
-            video.srcObject = stream;
-        })
-        .catch(err => {
-            console.error(err);
-        });
+    const select = document.createElement('select');
+    select.classList.add('device-select');
+    devices.forEach((device) => {
+        const option = document.createElement('option');
+        option.value = device.deviceId;
+        option.text = device.label || device.deviceId;
+        select.appendChild(option);
+    });
+
+    select.addEventListener('change', () => changeStreamDevice(select.value));
+
+    if (devices.length > 1) {
+        document.body.appendChild(select);
+    } else {
+        changeStreamDevice(devices[0].deviceId);
+    }
+
+    function changeStreamDevice(device) {
+        console.log('Changing stream device to:', device);
+        navigator.mediaDevices.getUserMedia({ video: { deviceId: device }, audio: false })
+            .then(stream => {
+                if (!video) return;
+                video.srcObject = stream;
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
 }
